e861c5a7c65965d1cd06cf53fc44bcecea00ea6a
Meena <meena.hoda@wmfs.net>
2025-03-18 07:09:13 +0000

    build(deps-dev): remove dependency @wmfs/tymly-solr-plugin

--- test/auth0-tests.js
+++ test/auth0-tests.js
@@ -35,8 +35,7 @@ describe('tymly-auth-auth0-plugin tests', function () {
         pluginPaths: [
           path.resolve(__dirname, './../lib'),
           require.resolve('@wmfs/tymly-rbac-plugin'),
-          require.resolve('@wmfs/tymly-cardscript-plugin'),
-          require.resolve('@wmfs/tymly-solr-plugin')
+          require.resolve('@wmfs/tymly-cardscript-plugin')
         ],
         blueprintPaths: [
           path.resolve(__dirname, './mock-system-blueprint')
